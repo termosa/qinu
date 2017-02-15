cf0710e1bbe64671e5a5649463e0587428a9ea1d
Stanislav Termosa <termosa.stanislav@gmail.com>
2017-02-15 19:37:04 +0200

    Add qinu library

---
+++ qinu.js
@@ -0,0 +1,62 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.qinu = factory();
+  }
+}(this, function () {
+
+  var defaultLength = 8;
+  var defaultTemplate = '%qinu%';
+  var defaultChars = [
+    1,2,3,4,5,6,7,8,9,0,
+    'a','b','c','d','e',
+    'f','g','h','i','j',
+    'k','l','m','n','o',
+    'p','q','r','s','t',
+    'u','v','w','x','y','z'
+  ];
+
+  function generateString(chars, length, string) {
+    if (typeof string === 'undefined') {
+      return generateString(chars, length, '');
+    }
+    if (string.length >= length) {
+      return string;
+    }
+    var randomIndex = Math.floor(Math.random() * chars.length);
+    var newChar = chars[randomIndex];
+    return generateString(chars, length, string + newChar);
+  }
+
+  function generate(opts, args) {
+    var randomString = generateString(opts.chars, opts.length);
+    var qinuString = opts.template.replace(/%qinu%/g, randomString);
+    for (var i = args.length; i--; ) {
+      qinuString = qinuString
+        .replace(new RegExp('%arg\\['+i+'\\]%', 'g'), args[i]);
+    }
+    return qinuString;
+  }
+
+  function qinu(options) {
+    var opts = {
+      length: options && options.length || defaultLength,
+      template: options && options.template || defaultTemplate,
+      chars: (options && options.chars || defaultChars).slice()
+    };
+    var args = Array.prototype.slice.call(arguments, 1);
+    if (options && options.args) {
+      args = options.args.concat(args);
+    }
+    return generate(opts, args);
+  }
+
+  qinu.create = function(opts) {
+    return qinu.bind(null, opts);
+  };
+
+  return qinu;
+}));
